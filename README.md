d78068a5d0dd3de73ad24927cb69f55a164b5307
jhiner <hinerman@gmail.com>
2016-10-13 14:47:18 -0400

    Works with pipeline2

---
+++ README.md
@@ -0,0 +1,11 @@
+# ndoejs-quickstart
+
+This is a prototype nodejs quickstart project to demonstrate new OAuth as a service features with Auth0.
+
+## Setup
+
+Rename .env.eaxmple to .env and update AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN, and AUTH0_CALLBACK_URL with your own values.
+
+## Running the example
+
+tbd
---
+++ routes/user.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var passport = require('passport');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var router = express.Router();
+var request = require('request');
+
+
+/* GET user profile. */
+router.get('/', ensureLoggedIn, function(req, res, next) {
+  res.render('user', {
+    user: req.user
+  });
+});
+
+router.get('/echo', ensureLoggedIn, function(req, res, next) {
+  
+  // invoke diag endpoint with authn header
+    var options = {
+      url: 'http://localhost:3000/diag',
+      headers: {
+        'Authorization': 'Bearer ' + req.user.extraParams.access_token
+      }
+    };
+
+    request(options, function(error, response, body) {
+        var info = JSON.parse(body);
+        res.json({jwt: info.jwt});
+    });
+
+});
+
+
+
+module.exports = router;
