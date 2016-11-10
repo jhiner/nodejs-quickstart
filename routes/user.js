910b4d14113abab081014a6ac1eba68c21beb0e9
jhiner <hinerman@gmail.com>
2016-11-09 21:42:35 -0500

    Updated refresh token work

--- routes/user.js
+++ routes/user.js
@@ -4,29 +4,66 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 var request = require('request');
 
+var apiData;
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
+  console.log('apiData: ' + apiData);
   res.render('user', {
-    user: req.user
+    user: req.user,
+    apiData: apiData
   });
 });
 
-router.get('/echo', ensureLoggedIn, function(req, res, next) {
+router.get('/apicall', ensureLoggedIn, function(req, res, next) {
   
   // invoke diag endpoint with authn header
     var options = {
-      url: 'http://localhost:3000/diag',
+      url: 'http://localhost:7000/authorized',
       headers: {
         'Authorization': 'Bearer ' + req.user.extraParams.access_token
       }
     };
 
     request(options, function(error, response, body) {
-        var info = JSON.parse(body);
-        res.json({jwt: info.jwt});
+      if (response.statusCode === 401) {
+        // need a refresh token
+        return res.redirect('/user/refresh');
+      }
+      
+      apiData = JSON.parse(body).data;
+      return res.redirect('/user');
     });
 
 });
+
+router.get('/refresh', ensureLoggedIn, function(req, res, next) {
+  // get refresh token
+  getRefreshToken(req, function (error, response, body) {
+    req.user.extraParams.id_token = body.id_token;
+    req.user.extraParams.access_token = body.access_token;
+    return res.redirect('/user/apicall');
+  });
+});
+
+function getRefreshToken(req, cb) {
+  // get refresh token
+  
+  var options = { method: 'POST',
+    url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+    headers: { 'content-type': 'application/json' },
+    body: { 
+      grant_type: 'refresh_token',
+      client_id: `${process.env.AUTH0_CLIENT_ID}`,
+      client_secret: `${process.env.AUTH0_CLIENT_SECRET}`,
+      refresh_token: req.user.extraParams.refresh_token,
+      redirect_uri: `${process.env.AUTH0_CALLBACK_URL}` 
+    }, 
+    json: true 
+  };
+
+  request(options, cb);
+
+}
 
 module.exports = router;
